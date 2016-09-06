b27d222789cfa698c6efe1bddaa5bc6e597973ae
liuyk <liuyk@yonyou.com>
2016-09-06 18:50:09 +0800

    去掉defin方式

--- vendor/ModuleBase.js
+++ vendor/ModuleBase.js
@@ -1,8 +1,6 @@
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define([], factory);
 	else {
 		var a = factory();
 		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
